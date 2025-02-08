66d57404076776a7e7d02d98af3af5d6954aaf40
Patrick Kerwood <patrick@kerwood.dk>
2025-02-08 21:46:02 +0100

    added support for current user label on page

--- src/error.rs
+++ src/error.rs
@@ -4,23 +4,26 @@ pub enum Error {
     #[error("ERROR: HTTP request, {0}")]
     Reqwest(#[from] reqwest::Error),
 
-    #[error("ERROR: Could not read the config file. {0}")]
+    #[error("ERROR: Could not read the config file: {0}")]
     IO(#[from] std::io::Error),
 
-    #[error("ERROR: Cound not parse YAML in config file, {0}")]
+    #[error("ERROR: Cound not parse YAML in config file: {0}")]
     SerdeYml(#[from] serde_yml::Error),
 
-    #[error("ERROR: Faild to parse string to interger; {0}")]
+    #[error("ERROR: Faild to parse string to interger: {0}")]
     ParseInt(#[from] std::num::ParseIntError),
 
-    #[error("ERROR: Cound not parse UTF-8 byte vector to String; {0}")]
+    #[error("ERROR: Cound not parse UTF-8 byte vector to String: {0}")]
     FromUtf8(#[from] std::string::FromUtf8Error),
 
     #[error("ERROR: No h1 header found in top of page. Add a header or use the overrideTitle configuration")]
     PageHeaderMissing,
 
-    #[error("ERROR: Protocol scheme (https://) missing from FQDN: [{0}]")]
+    #[error("ERROR: Protocol scheme is missing from FQDN: [{0}]")]
     ProtocolSchemeMissing(String),
+
+    #[error("ERROR: Failed to get the local part of the current user email.")]
+    CurrentUserEmailMissing,
 
     #[error(
         "ERROR: A confluence page ID annotation was found on a link, but could not be parsed [{0}]"
