c2a952a62afec63434e045a9862079fbceeecc71
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2025-11-11 15:24:43 +0100

    Add conditional logic for pa-token label in ConfluenceClient
    - Updated label creation to skip pa-token if user_label contains a dot.
    - Added a warning log for skipped labels.
    
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- src/confluence/client.rs
+++ src/confluence/client.rs
@@ -8,7 +8,7 @@ use reqwest::{
     ClientBuilder, Response,
 };
 use serde::Deserialize;
-use tracing::{debug, error, info, instrument, Level};
+use tracing::{debug, error, info, instrument, warn, Level};
 
 #[derive(Deserialize, Debug)]
 pub struct PageResponse {
@@ -226,10 +226,14 @@ impl ConfluenceClient {
             .0
             .to_string();
 
-        let labels = vec![
-            format!("page-sha/{}", page.page_sha),
-            format!("pa-token/{}", user_label),
-        ];
+        let mut labels = vec![format!("page-sha/{}", page.page_sha)];
+
+        // Add pa-token label only if user_label doesn't contain a dot
+        if user_label.contains('.') {
+            warn!("user label '{}' contains a dot and cannot be used as a Confluence label, skipping pa-token label", user_label);
+        } else {
+            labels.push(format!("pa-token/{}", user_label));
+        }
 
         for image_path in &page.html.image_paths {
             info!("uploading attachment [{}]", &image_path);
